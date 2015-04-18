378cbb1ed10750a84b87f7fa15ac9e7f81e66d76
Jeremiah Lin <cc.jeremiah.lin@gmail.com>
2015-04-18 14:48:43 -0700

    repeat customer mobile

---
+++ DA_RepeatCustomer_Mobile.R
@@ -0,0 +1,31 @@
+setwd("~/Google Drive/DataAnalytics/Transaction Data (updated)/SBI")
+library("plyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
+library("ggplot2", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
+
+txdata = read.csv("Dec2014_SBI.csv", header = TRUE)
+#txdata_1 = read.csv("JanD2C_2015.csv", header = TRUE)
+#txdata_2 = read.csv("FebD2C_2015.csv", header = TRUE)
+str(txdata)
+txdata$CSP_Code <- as.factor(txdata$CSP_Code)
+txdata$SBI_Account <- as.factor(txdata$SBI_Account)
+str(txdata)
+summary(txdata)
+rpCSP <- txdata[which(duplicated(txdata$CSP_Code)),]
+rpCSP <- txdata[which(txdata$CSP_Code %in% rpCSP$CSP_Code),]
+rpCustomer <- rpCSP[which(duplicated(rpCSP$Depositor_Mobile)),]
+rpCustomer <- rpCSP[which(rpCSP$Depositor_Mobile %in% rpCustomer$Depositor_Mobile),]
+rpCustomer <- rpCustomer[order(rpCustomer$Depositor_Mobile),]
+qplot()
+summary(rpCustomer)
+dim(rpCustomer)
+head(rpCSP[which(rpCSP$SBI_Account %in% rpCustomer$SBI_Account),], 5)
+#str(rpCustomer)
+qplot(RepeatTimes, Count, data=rpCustomer, geom = c("line","point"))
+rpDist <- count(rpCustomer, vars = "SBI_Account")
+rpCountDist <- count(rpDist, vars = "freq")
+colnames(rpCountDist) <- c("RepeatTimes", "Count")
+head(rpCountDist)
+qplot(RepeatTimes, Count, data=rpCountDist)
+qplot(RepeatTimes, Count, data=rpCountDist, size=Count)
+qplot(RepeatTimes, Count, data=rpCountDist, geom = c("line","point"))
+summary(rpDist)

90523580f49e87b8155c851c3cee48065c6d2bfe
Jeremiah Lin <cc.jeremiah.lin@gmail.com>
2015-04-18 17:31:45 -0700

    update

--- DA_RepeatCustomer_Mobile.R
+++ DA_RepeatCustomer_Mobile.R
@@ -10,22 +10,26 @@ txdata$CSP_Code <- as.factor(txdata$CSP_Code)
 txdata$SBI_Account <- as.factor(txdata$SBI_Account)
 str(txdata)
 summary(txdata)
+#get repeat CSP 
 rpCSP <- txdata[which(duplicated(txdata$CSP_Code)),]
 rpCSP <- txdata[which(txdata$CSP_Code %in% rpCSP$CSP_Code),]
+#get repeat Customer from repeat CSP
 rpCustomer <- rpCSP[which(duplicated(rpCSP$Depositor_Mobile)),]
 rpCustomer <- rpCSP[which(rpCSP$Depositor_Mobile %in% rpCustomer$Depositor_Mobile),]
+#sort repeat Cusomer who goes to the same CSP by Mobile Number (Customer ID)
 rpCustomer <- rpCustomer[order(rpCustomer$Depositor_Mobile),]
-qplot()
-summary(rpCustomer)
-dim(rpCustomer)
-head(rpCSP[which(rpCSP$SBI_Account %in% rpCustomer$SBI_Account),], 5)
-#str(rpCustomer)
-qplot(RepeatTimes, Count, data=rpCustomer, geom = c("line","point"))
-rpDist <- count(rpCustomer, vars = "SBI_Account")
+
+#for each CSP_Code, count the number of different Depositor_Mobile
+rpCustomer.csp_code.depositor_mb <- data.frame(rpCustomer$CSP_Code, rpCustomer$Depositor_Mobile)
+tbRpCs <- unique(rpCustomer.csp_code.depositor_mb)
+tbRpCSC <- count(tbRpCs, vars = "rpCustomer.CSP_Code")
+qplot(rpCustomer.CSP_Code, data=tbRpCs, geom="histogram")
+
+
+rpDist <- count(rpCustomer, vars = "Depositor_Mobile")
 rpCountDist <- count(rpDist, vars = "freq")
 colnames(rpCountDist) <- c("RepeatTimes", "Count")
-head(rpCountDist)
+
 qplot(RepeatTimes, Count, data=rpCountDist)
 qplot(RepeatTimes, Count, data=rpCountDist, size=Count)
 qplot(RepeatTimes, Count, data=rpCountDist, geom = c("line","point"))
-summary(rpDist)

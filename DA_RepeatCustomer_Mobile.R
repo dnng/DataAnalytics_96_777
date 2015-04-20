668d2534f4a198b216aef118e0b46263766d7178
Jeremiah Lin <cc.jeremiah.lin@gmail.com>
2015-04-19 22:33:39 -0700

    more on agents

--- DA_RepeatCustomer_Mobile.R
+++ DA_RepeatCustomer_Mobile.R
@@ -2,11 +2,16 @@ setwd("~/Google Drive/DataAnalytics/Transaction Data (updated)/SBI")
 library("plyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
 library("ggplot2", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
 library("qdapTools", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
-input_filename <- "JanD2C_2015.csv"
-output_filename <- "JanD2C_2015_SBI_Repeat_Customer_Pattern_by_Sender.csv"
-txdata = read.csv(input_filename, header = TRUE)
+library("openxlsx", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
+
+input_filename <- "DecD2C_2014.xlsx"
 #"JanD2C_2015.csv"
 #"FebD2C_2015.csv"
+txdata <- read.xlsx(input_filename, 1)  # read first sheet
+
+txdata$Tx_Time <- convertToDateTime(txdata$Tx_Time)
+
+output_filename <- "DecD2C_2014_SBI_Repeat_Customer_Pattern_by_Sender.csv"
 str(txdata)
 txdata$CSP_Code <- as.factor(txdata$CSP_Code)
 txdata$SBI_Account <- as.factor(txdata$SBI_Account)
@@ -17,18 +22,54 @@ summary(txdata)
 #   ===== get repeat customer (customer who has multiple transaction data)  ====
 fkCustomer <- txdata[which(duplicated(txdata$Depositor_Mobile)),]
 fkCustomer <- txdata[which(txdata$Depositor_Mobile %in% fkCustomer$Depositor_Mobile),]
-fkCustomer <- fkCustomer[order(fkCustomer$Depositor_Mobile),]
+fkCustomer <- fkCustomer[order(fkCustomer$Depositor_Mobile, fkCustomer$Tx_Time),]
 #   ===== get transaction data with the same customer ID, but different CSP_Code  ==== 
 agentsFkCustomer <- data.frame(fkCustomer$CSP_Code, fkCustomer$Depositor_Mobile)
 agentsFkCustomer <- unique(agentsFkCustomer)
 numOfAgentPerCustomer <- count(agentsFkCustomer, vars="fkCustomer.Depositor_Mobile")
-#realFickleCustomerID <- numOfAgentPerCustomer[which(numOfAgentPerCustomer$freq>1),]
-#realFickleCustomer <- fkCustomer[which(fkCustomer$Depositor_Mobile %in% realFickleCustomerID$fkCustomer.Depositor_Mobile),]
 fkCustomer$numOfSwitch <- lookup(fkCustomer$Depositor_Mobile, numOfAgentPerCustomer)
 table(fkCustomer$numOfSwitch)
 #combine rpCustomer and realFickleCustomer
 write.csv(fkCustomer, output_filename, row.names=FALSE)
 verifywriteOutput = read.csv(output_filename, header = TRUE)
+
+#txdata for agents who are switched
+txdataAgentSwitched <- fkCustomer[which(fkCustomer$numOfSwitch>1),]
+#NthAgent ===> Nth agent for the same customer.
+#switchMove ===> 1: first. 2: switch, 0: the same, -1 go back
+customerName <- 0
+agentName <- 0
+agentList <- data.frame(name="")
+for(i in 1:nrow(txdataAgentSwitched)){
+  if(txdataAgentSwitched[i,"Depositor_Mobile"]!=customerName){
+    customerName <- txdataAgentSwitched[i, "Depositor_Mobile"]
+    agentName <- txdataAgentSwitched[i, "CSP_Code"]
+    agentList <- data.frame(name=agentName)
+    txdataAgentSwitched[i, "switchMove"] <- "First"
+    txdataAgentSwitched[i, "NthAgent"] <- 1
+  }else{
+    tmpAgentName <- txdataAgentSwitched[i, "CSP_Code"]
+    if(tmpAgentName==agentName){
+      txdataAgentSwitched[i, "switchMove"] <- "The Same"
+      txdataAgentSwitched[i, "NthAgent"] <- which(agentList$name==tmpAgentName)
+    }else{
+      if((tmpAgentName %in% agentList$name)){
+        txdataAgentSwitched[i, "switchMove"] <- "Go Back"
+        txdataAgentSwitched[i, "NthAgent"] <- which(agentList$name==tmpAgentName)
+      }
+      else{
+        txdataAgentSwitched[i, "switchMove"] <- "Switch"
+        txdataAgentSwitched[i, "NthAgent"] <- nrow(agentList)+1
+        agentList[nrow(agentList)+1, "name"] <- tmpAgentName
+      }
+      agentName <- tmpAgentName
+    }
+  }
+}
+txdataAgentSwitched$switchMove <- as.factor(txdataAgentSwitched$switchMove)
+#agents who are switched from
+#agents who are switched to 
+
 
 # ===== for each CSP_Code, count the number of different Depositor_Mobile ======
 #rpCustomer.csp_code.depositor_mb <- data.frame(rpCustomer$CSP_Code, rpCustomer$Depositor_Mobile)
@@ -48,3 +89,4 @@ verifywriteOutput = read.csv(output_filename, header = TRUE)
 #big.vec <- sample(1:2, 3000000, TRUE)
 #out <- lookup(big.vec, key)
 #out[1:20]
+

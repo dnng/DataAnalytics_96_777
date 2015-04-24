80eb5a3b9f2b963e7826ea7df7f30f2e4a3aa35d
Jeremiah Lin <cc.jeremiah.lin@gmail.com>
2015-04-24 16:48:15 -0700

    update

--- DA_Agent_Same_Acount.R
+++ DA_Agent_Same_Acount.R
@@ -5,6 +5,7 @@ library("qdapTools", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resou
 library("openxlsx", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
 library("arules", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
 library("arulesViz", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
+library("data.table", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
 
 input_filename1 <- "DecD2C_2014.xlsx"
 input_filename2 <- "JanD2C_2015.xlsx"
@@ -25,9 +26,11 @@ txdata$CSP_Code <- as.factor(txdata$CSP_Code)
 
 # ===== get repeat SBI_Account from the same Agent =====
 
-# ========== get freq of combination ("CSP_Code", "SBI_Account") 
+# ========== get freq of combination ("CSP_Code", "SBI_Account")  and create feature of MoneyLauderyPotential
 count_RpSBI_Agent <- count(txdata, vars=c("CSP_Code", "SBI_Account"))
 count_RpSBI_Agent.sorted <- count_RpSBI_Agent[order(count_RpSBI_Agent$freq, decreasing = T),]
-setkey(count_RpSBI_Agent.sorted, )
-#txdata$AgentRpSBI_Count <- lookup(txdata$)
+count_RpSBI_Agent.table <- data.table(count_RpSBI_Agent.sorted)
+setkey(count_RpSBI_Agent.table, "CSP_Code", "SBI_Account")
+txdata$MoneyLauderyPotential <- count_RpSBI_Agent.table[J(txdata$CSP_Code, txdata$SBI_Account),]
+
 

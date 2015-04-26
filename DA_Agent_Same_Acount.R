02a627f189035ac3b7d31abae53982ba53016e39
Jeremiah Lin <cc.jeremiah.lin@gmail.com>
2015-04-25 17:03:55 -0700

    Update for Money Laundering Potential

--- DA_Agent_Same_Acount.R
+++ DA_Agent_Same_Acount.R
@@ -29,8 +29,20 @@ txdata$CSP_Code <- as.factor(txdata$CSP_Code)
 # ========== get freq of combination ("CSP_Code", "SBI_Account")  and create feature of MoneyLauderyPotential
 count_RpSBI_Agent <- count(txdata, vars=c("CSP_Code", "SBI_Account"))
 count_RpSBI_Agent.sorted <- count_RpSBI_Agent[order(count_RpSBI_Agent$freq, decreasing = T),]
+count_RpSBI_Agent.sorted$Agent_Account_CB <- paste(count_RpSBI_Agent.sorted$CSP_Code,count_RpSBI_Agent.sorted$SBI_Account)
 count_RpSBI_Agent.table <- data.table(count_RpSBI_Agent.sorted)
 setkey(count_RpSBI_Agent.table, "CSP_Code", "SBI_Account")
-txdata$MoneyLauderyPotential <- count_RpSBI_Agent.table[J(txdata$CSP_Code, txdata$SBI_Account),]
-
-
+txdata$MoneyLauderyPotential <- count_RpSBI_Agent.table[J(txdata$CSP_Code, txdata$SBI_Account), freq]
+txdata$Agent_Account_CB <- paste(txdata$CSP_Code, txdata$SBI_Account)
+# ==== plot the number transaction on the same agent/customer they made 
+largerThanNine <- count_RpSBI_Agent.sorted[which(count_RpSBI_Agent.sorted$freq>30),]
+barplot(largerThanNine$freq, col="Cyan", xlab="Different Agent Customer Combination", ylab="# of Transaction", axisnames = F)
+# ==== MoneyLauderyPotential Leader
+MnLauderyPotentialLeader <- txdata[which(txdata$MoneyLauderyPotential>=80),]
+MnLauderyPotentialLeader <- MnLauderyPotentialLeader[order(MnLauderyPotentialLeader$MoneyLauderyPotential, decreasing = T),]
+MnLauderyPotentialLeader$Tx_Time <- convertToDateTime(MnLauderyPotentialLeader$Tx_Time)
+MnLauderyPotentialLeader$WeekDay <- strftime(MnLauderyPotentialLeader$Tx_Time, format="%u")
+MnLauderyPotentialLeader$YearWeek <- strftime(MnLauderyPotentialLeader$Tx_Time, format="%y-%U")
+MnLauderyPotentialLeader$MoneyLauderyPotential <- as.factor(MnLauderyPotentialLeader$MoneyLauderyPotential)
+ggplot(MnLauderyPotentialLeader, aes(x=YearWeek, y=Amount, fill=WeekDay)) + geom_bar(stat= "identity") + facet_grid(. ~ MoneyLauderyPotential)
+  
